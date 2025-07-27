a6c12628091297feeac09b94efc54cd9ef866c99
changkhothuychung <nhat7203@gmail.com>
2025-07-27 16:19:43 -0400

    add basic test

---
+++ tests/beman/cache_latest/cache_latest.test.cpp
@@ -0,0 +1,15 @@
+#include <beman/cache_latest/cache_latest.hpp>
+#include <gtest>
+#include <ranges>
+#include <vector>
+
+TEST(CallTest, TestIterator) {
+    std::vector<int> v            = {1, 2, 3, 4, 5};
+    auto             even_squares = v | std::views::transform([](int i) {
+                            std::print("transform: {}\n", i);
+                            return i * i;
+                        }) |
+                        beman::views::cache_latest;
+    auto iter = even_squares.begin();
+    EXPECT_EQ(*iter, 1);
+}
